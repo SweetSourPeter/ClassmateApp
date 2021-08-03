f02df1bf70f0dcb82c76c4d6c4a56d071acdc61d
Hexuan Zhang <hexuanz@uvic.ca>
2021-08-02 18:46:23 -0700

    conflicts fix

--- lib/pages/group_chat_pages/courseDetail.dart
+++ lib/pages/group_chat_pages/courseDetail.dart
@@ -245,7 +245,7 @@ class _CourseDetailState extends State<CourseDetail> {
                         ),
                       ],
                     ),
-                  ),
+
             Container(
               color: riceColor,
               child: Column(
