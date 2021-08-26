38df85f7ae2abae7570ecc43ce4b61a891869082
Yaopu(Peter) Wang <49703445+SweetSourPeter@users.noreply.github.com>
2021-08-26 10:39:01 +0800

    change version

--- lib/pages/explore_pages/aboutTheApp.dart
+++ lib/pages/explore_pages/aboutTheApp.dart
@@ -51,7 +51,7 @@ class AboutTheAPP extends StatelessWidget {
                     height: mediaQuery.height * 0.0123,
                   ),
                   Text(
-                    'Version 1.1.7+12',
+                    'Version 1.1.7+13',
                     style: simpleTextStyle(Colors.white, 18),
                   ),
                   SizedBox(
